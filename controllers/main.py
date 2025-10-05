533f98737fd1e1ce9504b6f3cd4ac8e6ee17dc34
Muhamad Syarif <syarkev@gmail.com>
2025-10-05 19:16:24 +0700

    tes

--- controllers/main.py
+++ controllers/main.py
@@ -3,4 +3,4 @@ from odoo import http
 class TrainingController(http.Controller):
     @http.route('/training/hello', auth='public')
     def hello(self, **kw):
-        return "<h1>tes</h1>"
+        return "<h1>testing</h1>"
