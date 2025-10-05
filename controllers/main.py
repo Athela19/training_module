2e34bf54d678118b8a3eac547c7277db0b5408d7
Muhamad Syarif <syarkev@gmail.com>
2025-10-05 18:53:32 +0700

    tes

--- controllers/main.py
+++ controllers/main.py
@@ -3,4 +3,4 @@ from odoo import http
 class TrainingController(http.Controller):
     @http.route('/training/hello', auth='public')
     def hello(self, **kw):
-        return "<h1>Hello, Ini adalah versi pertama dari training module sebagai bukti adanya perubahan</h1>"
+        return "<h1>buahahaha</h1>"
